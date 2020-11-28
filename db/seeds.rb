e783ef6f4eff5331989a9d80a49c85eda90d3fd2
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2020-11-28 13:47:49 +0100

    new db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.destroy_all
-Cocktail.destroy_all
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
-# Ingredient.create(name: "wodka")
-# Ingredient.create(name: "gin")
+
+# Ingredient.destroy_all
+# Cocktail.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leave")
+Ingredient.create(name: "wodka")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "beer")
+Ingredient.create(name: "sparkling water")
+Ingredient.create(name: "maple syrup")
+Ingredient.create(name: "apple juice")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "parsley")
+Ingredient.create(name: "salt")
